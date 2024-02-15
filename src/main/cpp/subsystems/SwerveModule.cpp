159b4c0b3c7a307ad62aef2ac551b3cfc3bd1560
Kolby Balch <kolby.balch@gmail.com>
2024-02-15 17:06:23 -0600

    divide by max speed

--- src/main/cpp/subsystems/SwerveModule.cpp
+++ src/main/cpp/subsystems/SwerveModule.cpp
@@ -17,7 +17,7 @@ class SwerveModule {
             auto optimizedState = frc::SwerveModuleState::Optimize(desiredState,
                 units::radian_t(turnEncoder.GetAbsolutePosition()));
 
-            driveMotor.Set(double(optimizedState.speed));
+            driveMotor.Set(double(optimizedState.speed) / 4.877);
             turnMotor.Set(double(optimizedState.angle.Radians() / (2 * M_PI)));
         };
 
